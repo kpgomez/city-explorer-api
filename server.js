abba35198880f983b0c5c74dd9104acb3b6d227d
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-05-17 21:38:38 -0500

    removed console.logs

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.get('/search', getLocation)
 async function getLocation(req, res, next) {
     try {
         const { searchQuery } = req.query;
-        // console.log(searchQuery);
         const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&q=${searchQuery}&format=json`;
         let config = { headers: { Referer: process.env.SERVER_URL } }; //got help from Roger
         const locationResponse = await axios.get(url, config); //help from instructor
@@ -51,12 +50,9 @@ app.get('/weather', getLiveWeather);
 async function getLiveWeather(req, res, next) {
     try {
         const { lat, lon } = req.query;
-        // console.log(lat, lon);
         const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
         const weatherResponse = await axios.get(url);
-        // console.log(weatherResponse.data.data);
         const formattedWeatherData = weatherResponse.data.data.map(day => new Forecast(day));
-        // console.log(formattedWeatherData);
         res.status(200).send(formattedWeatherData);
     }
     catch (error) {
@@ -94,7 +90,11 @@ class Movie {
     constructor(obj) {
         this.title = obj.title;
         this.overview = obj.overview;
-        this.released = obj.release_date;
+        this.average_votes = obj.vote_average;
+        this.total_votes = obj.vote_count;
+        this.image_url = `https://image.tmdb.org/t/p/w500${obj.poster_path}`;
+        this.popularity = obj.popularity;
+        this.released_on = obj.release_date;
     }
 }
 
