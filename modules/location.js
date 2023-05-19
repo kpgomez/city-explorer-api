f6b93ad5929b6ca84b88fc4b056d0d336e47329f
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-05-18 21:48:56 -0500

    fixed response.data[0] on send to just response.data

--- modules/location.js
+++ modules/location.js
@@ -3,23 +3,12 @@
 const axios = require('axios');
 
 //because locationiq was moved to backend
-async function getLocation(req, res, next) {
-    // try {
-    //     const { searchQuery } = req.query;
-    //     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&q=${searchQuery}&format=json`;
-    //     let config = { headers: { Referer: process.env.SERVER_URL } }; //got help from Roger
-    //     const locationResponse = await axios.get(url, config); //help from instructor
-    //     res.send(locationResponse.data);
-    // }
-    // catch (error) {
-    //     next(error);
-    // }
-
+function getLocation(req, res, next) {
     const { searchQuery } = req.query;
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&q=${searchQuery}&format=json`;
     let config = { headers: { Referer: process.env.SERVER_URL } }; //got help from Roger
-    await axios.get(url, config)
-    .then(response => res.status(200).send(response.data[0])); //help from instructor
+    axios.get(url, config)
+    .then(response => res.status(200).send(response.data)); //help from instructor
 }
 
 module.exports = getLocation;
