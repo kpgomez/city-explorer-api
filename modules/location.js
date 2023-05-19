3425602bddb70d443088fb5f16b224efd8b705a9
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-05-18 23:37:55 -0500

    implemented cache for location module

--- modules/location.js
+++ modules/location.js
@@ -1,14 +1,33 @@
 'use strict'
 
 const axios = require('axios');
+const cache = require('./cache'); // I remmbered, yay!
 
 //because locationiq was moved to backend
 function getLocation(req, res, next) {
     const { searchQuery } = req.query;
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&q=${searchQuery}&format=json`;
-    let config = { headers: { Referer: process.env.SERVER_URL } }; //got help from Roger
-    axios.get(url, config)
-    .then(response => res.status(200).send(response.data)); //help from instructor
+
+    //unique identifier for the cache
+    const key = 'location' + searchQuery;
+
+    //checking if cache with key exists
+    if (cache[key]) {
+        console.log('cache hit - sending data from cache');
+        res.status(200).send(cache[key].data)
+    } else {
+        console.log('cache miss - making a new request to the API');
+        let config = { headers: { Referer: process.env.SERVER_URL } }; //got help from Roger
+        axios.get(url, config)
+            .then(response => {
+                cache[key] = {};
+                cache[key].data = response.data[0];
+                res.status(200).send(response.data[0]) //help from instructor
+                console.log(cache[key].data)
+            })
+            .catch(error => next(error));
+    }
+
 }
 
 module.exports = getLocation;
