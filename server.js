de90fe207cc07869c74eed9c7cc896e35475175e
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-05-18 20:35:41 -0500

    removed extra code

--- server.js
+++ server.js
@@ -53,34 +53,6 @@ app.get('/weather', getLiveWeather);
 //route for live movies
 app.get('/movies', getMovies);
 
-// async function getMovies(req, res, next) {
-//     try {
-//         const { searchQuery } = req.query;
-//         const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
-//         const moviesResponse = await axios.get(url);
-//         const formattedMovieData = moviesResponse.data.results.map(movie => new Movie(movie));
-//         res.status(200).send(formattedMovieData);
-//     }
-//     catch (error) {
-//         next(error)
-//     }
-// }
-
-
-
-//this class is used to format the movie data
-class Movie {
-    constructor(obj) {
-        this.title = obj.title;
-        this.overview = obj.overview;
-        this.average_votes = obj.vote_average;
-        this.total_votes = obj.vote_count;
-        this.image_url = `https://image.tmdb.org/t/p/w500${obj.poster_path}`;
-        this.popularity = obj.popularity;
-        this.released_on = obj.release_date;
-    }
-}
-
 //catch-all route
 app.get('*', (request, response) => {
     response.status(404).send('Not found');
